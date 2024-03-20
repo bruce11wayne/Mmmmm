59e6293686471f48fe07d707b4b4df9b8db21fb2
bruce11wayne <162586013+bruce11wayne@users.noreply.github.com>
2024-03-20 22:48:11 +0300

    questionhal.js

--- plugins/questionhal.js
+++ plugins/questionhal.js
@@ -5,7 +5,7 @@ handler.before = async function(m) {
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('لقد انتهى هذا سؤال الآن!');
+  if (!(id in this.tekateki)) return m.reply('لقد انتهى هذا سؤال الآن يا متعجرف!');
   if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
@@ -13,8 +13,8 @@ handler.before = async function(m) {
       m.reply(`*اجابة صحيحة!*\n+${this.tekateki[id][2]} Exp`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
-    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`اقتربت من الاجابة!`);
-    else m.reply('إجابة خاطئة!');
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`اقتربت من الاجابة تباً لك!`);
+    else m.reply('إجابة خاطئة يا وغد!');
   }
   return !0;
 };
